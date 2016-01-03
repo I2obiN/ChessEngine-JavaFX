d25f9f5975d02d17831e5de030a1730d01207900
Thomas Hood <thomashood10@yahoo.com>
2016-01-03 22:19:26 +0000

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -2,7 +2,7 @@ import javafx.scene.image.Image;
 
 //class declaration - abstract because we will not want to create a Piece object but we would
 //like to specify the private fields that all pieces should have in addition to their behaviours
-public abstract class Piece implements Cloneable {
+public abstract class Piece {
 	
 	//piece can be either white (1) or black (2)
 	private int type;
